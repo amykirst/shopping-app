92fd11565518bfdb270711a8640186b1a900df2e
amykirst <amylkirst@gmail.com>
2014-06-01 22:25:37 -0400

    Updated JS & HTML
    Removed placeholder list

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ $(document).ready(function() {
     	$("<li class='tile'>" + item + "<span class='delete'>Delete</span>" + "</li>").prependTo(".shopping_item_list");
     	$(".tile").removeClass("middle");   
     	$(".shopping_item_list li:nth-child(3n+2)").addClass("middle");
+    	//How do I reset form to placeholder text?
 	}); 
 
 
+	//delete list item
 	$(document).on("click", ".delete", function() {
 		$(this).closest("li.tile").remove(); 
 		$(".tile").removeClass("middle");	
