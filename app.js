1d6248036dd90bbcd6e157982a43e58af6479fb5
amykirst <amylkirst@gmail.com>
2014-06-01 21:47:20 -0400

    Updated JS
    Shopping app adds new item but does not apply styles

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
 
+	//add list item
+	$("input[name='shopping_item']").change(function() {
+    	var item = $(this).val();   
+    	$("<li class='tile'>" + item + "<span class='delete'>Delete</span>" + "</li>").prependTo(".shopping_item_list");
+    	$(".tile").removeClass("middle");   
+    	$(".shopping_item_list li:nth-child(3n+2)").addClass("middle");
+	}); 
+
+
 	// hide delete button
 	$(".delete").hide();
+
 
 	// delete square
 	$(".tile").hover(function() {
@@ -14,12 +24,11 @@ $(document).ready(function() {
 		$(".shopping_item_list li:nth-child(3n+2)").addClass("middle");
 	});
 
+
 	// cross off list 
 	$(".tile").click(function() {
 		$(this).toggleClass("deleteAction");
 	});
-
-	//add list item
 
 
 
