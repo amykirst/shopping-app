de564f051a962325977869ccb5dbcd823fcf08a0
Amy Kirst <amylkirst@gmail.com>
2014-10-05 10:32:35 -0400

    Updated JS
    Corrected trim function to check for blank entries

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
+// Troubleshooting delete and cross off:
+
+
+
 $(document).ready(function() {
   
   // Add list item
   $("input[name='shopping_item']").change(function(event) {
-    // Prevent browser from keeping list of suggestions and displaying them on input
-    event.preventDefault();
-    
-    // Store item
-    var item = $(this).val();
-    
-    // Check if entry is blank
-    if ($.trim(item) === "") {
-      event.preventDefault();
-      alert('Please enter an item.');
-    } else {
-      // If entry is not blank, add entry	
-      $("<li class='tile'>" + item +
-        "<span class='delete'>Delete</span>" + "</li>").prependTo(
-        ".shopping_item_list");
-      // Reset input field after user enters value
-      $("input[name='shopping_item']").val("");
-      // Remove middle class on reset to reassign middle to new middle item after addition
-      $(".tile").removeClass("middle");
-      // Add middle class to new middle items after addition
+    event.preventDefault(); // Prevents browser from keeping list of suggestions and displaying them on input
+      var item = $(this).val();   
+
+      // Check if string is emtpy
+      if($.trim(item) === "") {
+        alert("Please enter an item");
+      } else {
+
+      // Add list item if not empty
+      $("<li class='tile'>" + item + "<span class='delete'>Delete</span>" + "</li>").prependTo(".shopping_item_list");
+      $("input[name='shopping_item']").val(""); // Resets input field
+      $(".tile").removeClass("middle");   
       $(".shopping_item_list li:nth-child(3n+2)").addClass("middle");
       return false;
-    }
+      }
+  }); // end input function
     
     // Delete list item
     $("body").on("click", ".delete", function() {
+      //alert("The delete button was clicked");
       $(this).parent().remove();
       $(".tile").removeClass("middle");
       $(".shopping_item_list li:nth-child(3n+2)").addClass("middle");
@@ -38,5 +36,5 @@ $(document).ready(function() {
       $(this).toggleClass("deleteAction");
     });
     
-  }); // end input function
+ 
 }); // end of ready function
